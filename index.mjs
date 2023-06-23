8e7b62c6eb650067e5c62f1bac8959933eac5fac
AliSuliman <ali.sliman@ramaaz.com>
2023-06-23 15:53:11 +0300

    first commit

---
+++ index.mjs
@@ -0,0 +1,6 @@
+import Math from "./math.mjs"
+
+console.log(Math.add(3,2));
+console.log(Math.sub(3,2));
+console.log(Math.multiply(3,2));
+console.log(Math.divide(3,2));
---
+++ math.mjs
@@ -0,0 +1,20 @@
+
+const add = (a, b) => {
+    return a + b;
+}
+
+const sub = (a, b) => {
+    return a - b;
+}
+
+const multiply = (a, b) => {
+    return a * b;
+}
+
+const divide = (a, b) => {
+    return a / b;
+}
+
+export default {
+    add,sub,multiply,divide
+}
